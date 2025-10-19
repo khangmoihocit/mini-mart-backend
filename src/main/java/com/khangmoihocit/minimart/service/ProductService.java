7e038cb9f0a8feba850c1e8a36773f8814fcf0a3
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-10-19 10:26:17 +0700

    feat: add endpoint to update product images and optimize image handling

--- src/main/java/com/khangmoihocit/minimart/service/ProductService.java
+++ src/main/java/com/khangmoihocit/minimart/service/ProductService.java
@@ -2,7 +2,12 @@ package com.khangmoihocit.minimart.service;
 
 import com.khangmoihocit.minimart.dto.request.ProductRequest;
 import com.khangmoihocit.minimart.dto.response.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface ProductService extends BaseCRUDService<ProductResponse, ProductRequest> {
     ProductResponse createProductWithImages(ProductRequest request);
+
+    ProductResponse updateProductImages(String id, List<MultipartFile> files);
 }
